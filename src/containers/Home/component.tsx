5d9f1c9fb5898992ace77978851fd2f4f85aa9e7
5h1ngy <sig.scarano@outlook.it>
2024-12-01 04:03:39 +0100

    chore: :fire: road to persistent storage

--- src/containers/Home/component.tsx
+++ src/containers/Home/component.tsx
@@ -42,6 +42,8 @@ const Component: React.FC<Props & WithRouterProps> = ({ router, state, actions, 
             { label: "50", value: "50" },
             { label: "100", value: "100" },
             { label: "200", value: "200" },
+            { label: "500", value: "500" },
+            { label: "1000", value: "1000" },
         ],
     })
 
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import pageLanding from './pageLanding';
 import containerHome from './containerHome';
@@ -6,6 +8,25 @@ import containerDetails from './containerDetails';
 import { actions as pageLandingActions } from "./pageLanding"
 import { actions as containerHomeActions } from "./containerHome"
 import { actions as containerDetailsActions } from "./containerDetails"
+
+// const persistConfig = {
+//     key: "root",
+//     storage,
+// };
+
+// const persistedReducer = persistReducer(persistConfig, userReducer);
+// export const store = configureStore({
+//     reducer: persistedReducer,
+//     devTools: process.env.NODE_ENV !== "production",
+//     middleware: (getDefaultMiddleware) =>
+//         getDefaultMiddleware({
+//             serializableCheck: {
+//                 ignoredActions: ["persist/PERSIST", "persist/REHYDRATE"],
+//             },
+//         }),
+// });
+
+// export const persistor = persistStore(store);
 
 export const actions = {
     pageLanding: pageLandingActions,
